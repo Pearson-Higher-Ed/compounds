318ef01743bb61b3178e62ca4f159573cb9cdf18
Adam Armour <adam.armour@gmail.com>
2015-12-29 11:44:39 -0700

    refactor: export all modules instead of exporting each explicitly
    export * broke when upgrading to Babel 6 but has been fixed.

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// TODO: export * stopped working with Babel 6. Figure out why
-// so that we don't need to manually re-export everything.
-// See https://phabricator.babeljs.io/T2763
-//
-// export * from './src';
-
-export { Button } from './src';
-export { Card } from './src';
-export { CardHeader } from './src';
-export { CardContent } from './src';
-export { Label } from './src';
-export { Lead } from './src';
-export { Title } from './src';
+export * from './src';
