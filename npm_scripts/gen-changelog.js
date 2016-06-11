3fe884dbb71150e1b556a855782fae624b4c70ce
Kaka, Aaron A <Aaron.Kaka@pearson.com>
2016-06-11 13:33:09 -0600

    chore: Prep Compounds SDK for publishing to npm.

---
+++ npm_scripts/gen-changelog.js
@@ -0,0 +1,18 @@
+const changelog = require('conventional-changelog');
+const fs = require('fs');
+
+const FILENAME = 'CHANGELOG.md';
+const REGENERATE_ALL = 0;
+
+const outStream = fs.createWriteStream(FILENAME);
+
+const changelogStream = changelog({
+  preset: 'angular',
+  releaseCount: REGENERATE_ALL
+});
+
+changelogStream.pipe(outStream);
+
+changelogStream.on('end', () => {
+  console.log('Changelog saved to', FILENAME);
+});
---
+++ npm_scripts/release.js
@@ -0,0 +1,72 @@
+// Execute this script in the target branch to release to npm!
+
+const exec = require('./exec');
+const readline = require('readline');
+const semver = require('semver');
+const pkg = require('../package.json');
+const branchName = exec('git rev-parse --abbrev-ref HEAD', true);
+const currentVersion = pkg.version;
+const stdin = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const syncRemote = (branchName, nextVersion) => {
+
+  exec(`git push origin ${branchName}`);
+
+  if (nextVersion) {
+    exec(`git push --tags`);
+    console.log(`TravisCI will now release to npm on the tagged commit ${nextVersion} for the pearson-ux account.`);
+  }
+};
+const exitFailure = (message) => {
+  console.error(message);
+  process.exit(1);
+};
+
+if (!branchName.startsWith('v')) {
+  exitFailure('You must be on the official version branch in order to release to npm.');
+}
+
+// *** Releaser provides the target SEMVER-compliant version ***
+
+stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+
+  if (!semver.valid(nextVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: requires a semver-compliant version. See http://semver.org/`);
+  }
+
+  if (!semver.gt(nextVersion, currentVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
+  }
+
+  if (nextVersion.startsWith('v')) {
+    nextVersion = nextVersion.slice(1);
+  }
+
+  // Ensure unit tests pass before continuing!
+  exec('npm test');
+
+  // Ensure the build will successfully generate
+  exec('npm run build-dist');
+
+  // Order of operations:
+  // 1. Bump the version update in package.json and npm-shrinkwrap.json
+  // 2. The 'version' custom npm script (defined in package.json) executes changelog generation and adding to commit
+  // 3. Locally commit and tag
+  exec(`npm version ${nextVersion}`);
+
+  // push commit and tag on target release branch
+  syncRemote(branchName, nextVersion);
+
+  // Generate gh-pages branch and sync with remote
+  exec('npm run gh-pages');
+  exec('git pull -s recursive -Xours --no-edit');
+  syncRemote('gh-pages');
+
+  // Go back from whence you came
+  exec(`git checkout ${branchName}`);
+
+});
+
+stdin.close();
