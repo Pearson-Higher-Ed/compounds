8a61029ec8c3e165e359a7c32871aeeff38e369e
gayan <gayan.kumara@pearson.com>
2016-07-28 11:25:02 +0530

    feat: date format localization for date picker

--- docs/src/app/components/containers/components/DatePicker.js
+++ docs/src/app/components/containers/components/DatePicker.js
@@ -93,7 +93,8 @@ export default class DatePicker extends React.Component {
           <div className="pe-input-group pe-input-icon">
           <div className="pe-input" style={divStyle1}>
            <Onclickout onClickOut={this.handleOnClickOutside}>
-           <input ref="input" type="text" id="i2" placeholder="mm/dd/yyyy"
+           <input ref="input" type="text" id="i2"
+           placeholder={(moment.localeData(this.props.locale)._longDateFormat.L).toLowerCase()}
            onClick={this.handleOnClick}  value={this.state.value}
            onChange={this.handleInputChange}
            onFocus={this.handleOnClick} />
--- src/calendar/DatePicker.js
+++ src/calendar/DatePicker.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import DatePicker from '../../docs/src/app/components/containers/components/DatePicker';
+import moment from 'moment';
 
 function DatePickerCalendar(props) {
+
+  if (moment.localeData(props.params.locale) === null) {
+    props.params.locale = 'en';
+  }
+
   return (
      <DatePicker locale={props.params.locale}/>
    );
