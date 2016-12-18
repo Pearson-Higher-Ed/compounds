9b13080ba90bde38f91d3bab6e090d26677049ed
JasonLantz <lantjason@yahoo.com>
2016-12-17 20:18:08 -0700

    fix:move stuff

--- src/Button.js
+++ src/Button.js
@@ -4,37 +4,27 @@ import React from 'react';
 const Button = (props) => {
 
   const { btnType, btnSize, ...rest } = props;
-  const classes = generateClassName(btnType, btnSize);
 
-  return <button className={classes} {...rest}>{props.children}</button>;
-};
-
-export default Button;
-
-
-
-
-const generateClassName = (btnType, btnSize) => {
-
-  let className;
+  let classes;
 
   if (!btnType) {
     if (!btnSize) {
-      className = `pe-btn`;
-    }
-    if (btnSize) {
-      className = `pe-btn--btn_${btnSize}`;
+      classes = `pe-btn`;
+    } else {
+      classes = `pe-btn--btn_${btnSize}`;
     }
   };
 
   if (btnType) {
     if (!btnSize) {
-      className = `pe-btn__${btnType}`;
-    }
-    if (btnSize) {
-      className = `pe-btn__${btnType}--btn_${btnSize}`;
+      classes = `pe-btn__${btnType}`;
+    } else {
+      classes = `pe-btn__${btnType}--btn_${btnSize}`;
     }
   };
 
-  return className;
-}
+  return <button className={classes} {...rest}>{props.children}</button>;
+};
+
+
+export default Button;
