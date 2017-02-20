d647badffcfb77f04fe9a015f57d8c5e1fd5ea77
ulantja <Jason.Lantz@pearson.com>
2017-02-20 00:10:15 -0700

    chore: pause it

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     path          : path.resolve(__dirname, 'build'),
-    filename      : '[name].compounds.js',
+    filename      : '[name].compounds.[hash].js',
     publicPath    : 'http://pearson-higher-ed.github.io/compounds',
     libraryTarget : 'umd'
   },
