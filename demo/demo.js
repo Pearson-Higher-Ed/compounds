ea91eb3981cdff9432481a192146f6c627aaf8fa
ulantja <Jason.Lantz@pearson.com>
2017-03-06 12:17:52 -0700

    chore: check routing........+++++++-

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,5 @@
-import React            from 'react';
-import ReactDOM         from 'react-dom';
+import React         from 'react';
+import ReactDOM      from 'react-dom';
 import ButtonPage    from './demoPages/ButtonPage';
 import IconPage      from './demoPages/IconPage';
 import HomePage      from './demoPages/HomePage';
@@ -7,18 +7,17 @@ import HeaderPage    from './demoPages/HeaderPage';
 import InputsPage    from './demoPages/InputsPage';
 import NotFoundPage  from './demoPages/NotFoundPage';
 
-import { BrowserRouter as Router, Route, IndexRoute } from 'react-router-dom';
+import { HashRouter as Router, Route, IndexRoute } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <Router basename="/compounds">
+    <Router basename="/">
       <div>
         <HeaderPage />
           <Route exact path="/"  component={HomePage}     />
           <Route path="/icons"   component={IconPage}     />
           <Route path="/inputs"  component={InputsPage}   />
           <Route path="/buttons"  component={ButtonPage}   />
-          <Route path="*" component={NotFoundPage} />
       </div>
     </Router>, document.getElementById('app')
 );
