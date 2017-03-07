0cd5fd1e1550a1147ba95244b05a3ac6423f8caa
ulantja <Jason.Lantz@pearson.com>
2017-03-07 09:57:36 -0700

    fix: add build and coverage to gitignore

--- build/dist.compounds.js
+++
@@ -1,2 +0,0 @@
-!function(e,o){if("object"==typeof exports&&"object"==typeof module)module.exports=o();else if("function"==typeof define&&define.amd)define([],o);else{var t=o();for(var n in t)("object"==typeof exports?exports:e)[n]=t[n]}}(this,function(){return webpackJsonp([3,4],{3:function(e,o,t){e.exports=t("./Compounds.js")}},[3])});
-//# sourceMappingURL=dist.compounds.js.map
