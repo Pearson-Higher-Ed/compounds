c4984b77da29429272e508c2e55e4f01b28f919a
ulantja <Jason.Lantz@pearson.com>
2017-03-10 14:07:03 -0700

    fix: forgot to rename compounds to index in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const index                 = `${__dirname}/index.html`;
 const demo                  = `${__dirname}/demo/demo.js`;
 const demoScss              = `${__dirname}/demo/demo.scss`;
 const main                  = `${__dirname}/demo/main.js`;
-const compounds             = `${__dirname}/Compounds.js`;
+const compounds             = `${__dirname}/index.js`;
 const icons                 = `${__dirname}/node_modules/pearson-elements/dist/icons/p-icons-sprite-1.1.svg`;
 const elements              = `${__dirname}/node_modules/pearson-elements/dist/css/elements.css`;
 
