8fcf8be6f30bd6312f796a772131cc65c048f059
ulantja <Jason.Lantz@pearson.com>
2017-03-24 14:07:33 -0600

    chore: wire up intl

--- demo/demoPages/InputsPage.js
+++ demo/demoPages/InputsPage.js
@@ -1,5 +1,5 @@
-import React         from 'react';
-import { TextInput } from '../../index';
+import React                        from 'react';
+import { TextInput, PasswordInput } from '../../index';
 
 
 const InputsPage = () => (
@@ -63,6 +63,34 @@ const InputsPage = () => (
 
           <br />
           <br />
+
+            <h1><a href="http://pearson-higher-ed.github.io/design/c/inputs/beta/#specialized-inputs">Password Input</a></h1>
+
+              <h2>Default:</h2>
+              <PasswordInput id="e" placeholder="Enter Password" showText="show" hideText="hide" />
+              <p className="code">{'<PasswordInput id="a" placeholder="Enter Password" showText="show" hideText="hide" />'}</p>
+
+
+              <br />
+              <br />
+
+
+              <h2>Custom Label:</h2>
+              <PasswordInput id="f" label="Password" placeholder="Enter Password" showText="show" hideText="hide" />
+              <p className="code">{'<PasswordInput id="a" label="Password" placeholder="Enter Password" showText="show" hideText="hide" />'}</p>
+
+
+              <br />
+              <br />
+
+
+              <h2>Error:</h2>
+              <PasswordInput id="h" label="Password" placeholder="Enter Password" error={true} showText="show" hideText="hide" />
+              <p className="code">{'<PasswordInput id="h" label="Password" placeholder="Enter Password" error={true} showText="show" hideText="hide" />'}</p>
+
+
+              <br />
+              <br />
 
 
         </div>
