c952edea05144b14dad4558e704adacd3740a5c5
ulantja <Jason.Lantz@pearson.com>
2017-03-24 22:40:59 -0600

    fix: move test files

--- test/PasswordInput.test.js
+++ test/PasswordInput.test.js
@@ -1,7 +1,7 @@
 import React             from 'react';
 import expect            from 'expect';
 import { shallow }       from 'enzyme';
-import { PasswordInput } from '../../index';
+import { PasswordInput } from '../index';
 
 describe('PasswordInput', () => {
 
