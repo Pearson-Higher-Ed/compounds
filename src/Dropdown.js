eda5404c79117fe313c2e21e39776a757e2a149b
Mike <Mhomolak@yahoo.com>
2017-03-28 13:28:32 -0600

    chore: basic dropdown | update elements

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,15 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 
+import './Dropdown.scss';
+
 class Dropdown extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      open: false
+    }
+
+  }
+
+  _handleDropDown() {
+    this.setState({
+      open: !this.state.open
+    })
   }
 
   render() {
     return(
       <div>
-        <p>Hey, dropdown.</p>
+        <div onClick={this._handleDropDown.bind(this)}>
+          <button>Toggle</button>
+          { this.state.open
+            ?
+            <ul className="li-wrapper">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+
+            : null
+          }
+        </div>
+
       </div>
     )
   }
