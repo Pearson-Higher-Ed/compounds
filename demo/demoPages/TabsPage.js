7630af8dad80cbacf745eb0e257412535f2574e7
Mike <Mhomolak@yahoo.com>
2017-04-20 13:37:41 -0600

    chore: piecing together Tabs

--- demo/demoPages/TabsPage.js
+++ demo/demoPages/TabsPage.js
@@ -3,7 +3,17 @@ import { Tabs } from '../../index';
 
 const TabsPage = () => (
   <div>
-    <Tabs />
+    <Tabs>
+      <Pane label="Tab 1">
+        <div>Tab 1 contents</div>
+      </Pane>
+      <Pane label="Tab 2">
+        <div>Tab 2 contents</div>
+      </Pane>
+      <Pane label="Tab 3">
+        <div>Tab 3 contents</div>
+      </Pane>
+    </Tabs>
   </div>
 );
 
--- src/Tabs.js
+++ src/Tabs.js
@@ -2,15 +2,49 @@ import React, { Component } from 'react';
 
 class Tabs extends Component {
 
+  static defaultProps = {
+    selected: 0
+  }
+
   constructor(props) {
     super(props)
+
+    this.state = {
+      selected: this.props.selected
+    }
   };
+
+  _renderContent() {
+    return (
+      <div className="tabs__content">
+        {this.props.children[this.state.selected]}
+      </div>
+    );
+  }
+
+  _renderTitles() {
+    function labels(child, index) {
+      return (
+        <li key={index}>
+          <a href="#">
+            {child.props.label}
+          </a>
+        </li>
+      );
+    }
+    return (
+      <ul className="tabs__labels">
+        {this.props.children.map(labels.bind(this))}
+      </ul>
+    );
+  }
 
   render() {
 
     return (
-      <div>
-        <p>Hi</p>
+      <div className="tabs">
+        {this._renderTitles()}
+        {this._renderContent()}
       </div>
     )
   };
