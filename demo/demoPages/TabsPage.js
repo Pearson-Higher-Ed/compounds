a3d785be97bbb425ce80de9dcb9b90be34e42670
Mike <Mhomolak@yahoo.com>
2017-04-21 10:18:23 -0600

    chore: minor styles

--- demo/demoPages/TabsPage.js
+++ demo/demoPages/TabsPage.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { Tabs, Pane } from '../../index';
 
 const TabsPage = () => (
-  <div>
-    <Tabs>
-      <Pane label="Tabby">
-        <div>Tab 1</div>
-      </Pane>
-      <Pane label="Tabetha">
-        <div>Tab 2</div>
-      </Pane>
-      <Pane label="Tabberino">
-        <div>Tab 3</div>
-      </Pane>
-    </Tabs>
-  </div>
+  <Tabs>
+    <Pane label="Tabby">
+      <div>Tab 1</div>
+    </Pane>
+    <Pane label="Tabetha">
+      <div>Tab 2</div>
+    </Pane>
+    <Pane label="Tabberino">
+      <div>Tab 3</div>
+    </Pane>
+  </Tabs>
 );
 
 export default TabsPage;
