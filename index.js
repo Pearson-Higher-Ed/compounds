e6b52519a173afbd04fc740bdc7643b8a998f38b
Mike <Mhomolak@yahoo.com>
2017-04-25 13:33:12 -0600

    align branches

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ export Icon          from './src/Icon';
 export PasswordInput from './src/PasswordInput';
 export TextInput     from './src/TextInput';
 export Dropdown      from './src/Dropdown';
+
+// Tabs
 export Tabs          from './src/Tabs';
 export Pane          from './src/Pane';
