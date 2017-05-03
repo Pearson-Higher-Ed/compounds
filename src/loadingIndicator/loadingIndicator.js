94d27380d0fa866e54b7c4a9d187039f6734e9b1
Jonathan Wyse <jonathan.wyse@pearson.com>
2017-05-03 15:31:41 -0600

    more work on loading indicator

--- src/loadingIndicator/loadingIndicator.js
+++ src/loadingIndicator/loadingIndicator.js
@@ -11,9 +11,13 @@ class LoadingIndicator extends Component {
     return(
       <div className="pe-loadingIndicator">
         <div className="pe-loadingIndcator-overlay"></div>
-        <div className="pe-loadingIndicator-box">
-          <LoadingSpinner />
-          <div className="pe-loadingIndicator-text">Loading...</div>
+        <div className="pe-loadingIndicator-chip">
+          <div className="pe-loadingIndicator-contents">
+            <div className="pe-loadingIndicator-spinner">
+              <LoadingSpinner />
+            </div>
+            <div className="pe-loadingIndicator-text">Loading...</div>
+          </div>
         </div>
       </div>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path              = require('path');
 const webpack           = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const demo              = `${__dirname}/demo/demo.js`;
 const demoScss          = `${__dirname}/demo/demo.scss`;
 const main              = `${__dirname}/demo/main.js`;
@@ -9,12 +8,13 @@ const compounds         = `${__dirname}/index.js`;
 const icons             = `${__dirname}/node_modules/pearson-elements/dist/icons/p-icons-sprite-1.1.svg`;
 const elements          = `${__dirname}/node_modules/pearson-elements/dist/css/elements.css`;
 const loadingSpinner    = `${__dirname}/src/loadingSpinner/loadingSpinner.scss`;
+const loadingIndicator  = `${__dirname}/src/loadingIndicator/loadingIndicator.scss`;
 
 
 module.exports = {
   entry: {
     demo   : [ demo, demoScss ],
-    dev    : [ elements, icons, main, loadingSpinner ],
+    dev    : [ elements, icons, main, loadingSpinner, loadingIndicator ],
     dist   : [ compounds ]
   },
   output: {
@@ -23,11 +23,11 @@ module.exports = {
     publicPath    : '/compounds/',
     libraryTarget : 'umd'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
-    host               : "0.0.0.0",
+    host               : '0.0.0.0',
     port               : 8081,
-    publicPath         : "/compounds/",
+    publicPath         : '/compounds/',
     https              : true,
     overlay            : true,
     watchContentBase   : true,
@@ -35,50 +35,50 @@ module.exports = {
     watchOptions       : { poll: true }
   },
   externals: [
-      {
-        react: {
-          root: 'React',
-          commonjs2: 'react',
-          commonjs: 'react',
-          amd: 'react'
-        },
-        'react-dom': {
-          root: 'ReactDOM',
-          commonjs2: 'react-dom',
-          commonjs: 'react-dom',
-          amd: 'react-dom'
-        }
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
       }
+    }
   ],
   module: {
     rules: [
-        {
-          test: /\.(css|scss)$/,
-          use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
-          }]
+      {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
 
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ["es2015", "stage-0", "react"]
-          }
-        },
-        {
-          test: /\.(png|jpg|gif|svg|ttf|woff|woff2)$/,
-          loader: 'file-loader',
-          options: {
-              name: '[name].[ext]?[hash]'
-          }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'stage-0', 'react']
         }
-      ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
