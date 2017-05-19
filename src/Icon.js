5b077ed47aa16b0b350fa60971d888172e9bacd8
ulantja <Jason.Lantz@pearson.com>
2017-05-19 14:48:49 -0600

    fix: padding issues

--- src/Icon.js
+++ src/Icon.js
@@ -7,21 +7,14 @@ const Icon = (props) => {
 
   const { name, children } = props;
 
-  const icon_class = 'pe-icon--' + name;
-  const hidden     = (children) ? false         : true;
-  const roleAttr   = (children) ? "img"         : null;
-  const i_id       = (children) ? '_'+uuid.v1() : null;
-
+  const i_id = (children) ? '_'+uuid.v1() : null;
 
   return(
-      <svg version         = "1.1"
-           xmlns           = "http://www.w3.org/2000/svg"
-           xmlnsXlink      = "http://www.w3.org/1999/xlink"
-           focusable       = "false"
-           role            = {roleAttr}
-           aria-hidden     = {hidden}
+      <svg focusable       = "false"
+           role            = {children ? "img" : null}
+           aria-hidden     = {children ? false : true}
            aria-labelledby = {i_id}
-           className       = {icon_class}
+           className       = {`pe-icon--${name}`}
            >
            {children && <title id={i_id}>{children}</title>}
         <use xlinkHref={'#' + name}></use>
--- src/Modal.js
+++ src/Modal.js
@@ -18,9 +18,15 @@ class Modal extends Component {
       cancelBtnHandler  : this.props.cancelBtnHandler
     };
 
-    this.onClose      = _onClose.bind(this);
-    this.renderFooter = _renderFooter.bind(this);
-    this.afterOpen    = _afterOpen.bind(this);
+
+    this.onClose            = _onClose.bind(this);
+    this.renderFooter       = _renderFooter.bind(this);
+    this.afterOpen          = _afterOpen.bind(this);
+    this.applyWrapper       = _applyWrapper.bind(this);
+    this.removeWrapper      = _removeWrapper.bind(this);
+    this.successBtnHandler  = _successBtnHandler.bind(this);
+    this.cancelBtnHandler   = _cancelBtnHandler.bind(this);
+    this.removeOverlayStyle = _removeOverlayStyle.bind(this);
 
 
     // expose modal via event...
@@ -38,6 +44,17 @@ class Modal extends Component {
   };
 
 
+  componentWillReceiveProps(nextProps){
+    this.setState({modalIsOpen:nextProps.isShown});
+    if(!this.state.modalIsOpen && document.getElementsByClassName('modalOverlay')[0]){
+      this.removeOverlayStyle();
+      this.removeWrapper();
+    }
+    if(!this.state.modalIsOpen){
+      this.applyWrapper();
+    }
+  }
+
   render() {
 
     const { modalIsOpen, footerVisible, text, children, successBtnHandler, cancelBtnHandler } = this.state;
@@ -46,16 +63,16 @@ class Modal extends Component {
         <BaseModal
           className        = "pe-template__static-medium modalContent"
           overlayClassName = "modalOverlay"
-          isOpen           = {modalIsOpen || this.props.isShown}
+          isOpen           = {modalIsOpen}
           onAfterOpen      = {this.afterOpen}
           onRequestClose   = {this.onClose}
-          ariaHideApp      = {false}
+          ariaHideApp      = {true}
           role             = "dialog"
           contentLabel     = "Modal"
   	      >
 
           <div id="modalHeader" className="modalHeader">
-            {!footerVisible && <button className="modalClose pe-icon--btn" onClick={cancelBtnHandler}>
+            {!footerVisible && <button className="modalClose pe-icon--btn" onClick={this.cancelBtnHandler}>
               <Icon name='remove-sm-24'>{text.closeButtonSRText}</Icon>
             </button>}
             {text.headerTitle && <h2 id="modalHeaderText" className="modalHeaderText pe-title">{text.headerTitle}</h2>}
@@ -88,10 +105,23 @@ Modal.propTypes = {
 
 export function _onClose() {
   const modalOverlay = document.getElementsByClassName('modalOverlay')[0];
-  !this.state.modalIsOpen ? null : modalOverlay.style = "";
-  this.setState({modalIsOpen:false});
+  modalOverlay.style = "";
+  this.cancelBtnHandler();
+}
+
+export function _successBtnHandler() {
+  this.state.successBtnHandler.call(this);
+}
+
+export function _cancelBtnHandler() {
+  this.removeOverlayStyle();
   this.state.cancelBtnHandler.call(this);
-};
+}
+
+export function _removeOverlayStyle(){
+  const modalOverlay = document.getElementsByClassName('modalOverlay')[0];
+  modalOverlay.style = "";
+}
 
 export function _afterOpen() {
 
@@ -105,9 +135,6 @@ export function _afterOpen() {
 
   // apply Focus to close button on open...
   headerCloseButton ? headerCloseButton.focus() : footerCloseButton.focus();
-
-  // set aria values...
-  modalContent.setAttribute('aria-labelledby', 'modalContent');
 
   // apply padding based on clientHeight...
   const windowHeight  = window.innerHeight;
@@ -130,12 +157,43 @@ export function _afterOpen() {
 
 };
 
+export function _applyWrapper() {
+
+  if (!document.getElementById('wrapper')) {
+
+    const wrapper = document.createElement('div');
+    wrapper.id    = 'wrapper';
+    wrapper.setAttribute('aria-hidden', true);
+
+    const excludedElement = document.getElementsByClassName('ReactModalPortal')[0];
+    while (document.body.firstChild !== excludedElement) {
+      wrapper.appendChild(document.body.firstChild);
+    }
+
+    document.body.appendChild(wrapper);
+    document.body.appendChild(excludedElement);
+  }
+
+};
+
+export function _removeWrapper() {
+  const wrapper         = document.getElementById('wrapper');
+  const excludedElement = document.getElementsByClassName('ReactModalPortal')[0];
+
+  while (wrapper.firstChild) {
+    document.body.appendChild(wrapper.firstChild);
+  }
+
+  document.body.removeChild(wrapper);
+  document.body.appendChild(excludedElement);
+};
+
 export function _renderFooter(footerVisible, text, successBtnHandler, cancelBtnHandler) {
   if (footerVisible) {
     return(
       <div className="modalFooter" >
-        <button onClick={cancelBtnHandler.bind(this)} className="modalCancel pe-btn--btn_large">{text.modalCancelButtonText}</button>
-        <button onClick={successBtnHandler.bind(this)} className="modalSave pe-btn__cta_t--btn_large">{text.modalSaveButtonText}</button>
+        <button onClick={this.cancelBtnHandler} className="modalCancel pe-btn--btn_large">{text.modalCancelButtonText}</button>
+        <button onClick={this.successBtnHandler} className="modalSave pe-btn__cta_t--btn_large">{text.modalSaveButtonText}</button>
       </div>
     )
   };
