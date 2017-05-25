20c708cd23039fad3f23eed511c44f7668b9dc88
Mike <Mhomolak@yahoo.com>
2017-05-25 09:23:14 -0600

    chore: placeholder

--- demo/demoPages/StaticAlertPage.js
+++ demo/demoPages/StaticAlertPage.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { StaticAlert } from '../../index';
 
 const StaticAlertPage = () => (
-  <StaticAlert />
-)
-
+  <StaticAlert type="Error" title="Test" message="Just some test text" />
+);
 
 
 export default StaticAlertPage;
--- src/StaticAlert/StaticAlert.js
+++ src/StaticAlert/StaticAlert.js
@@ -17,7 +17,7 @@ export default class StaticAlert extends Component {
 
     this.state = {
       isOpen: true,
-      opacity: 1
+      display: 'block'
     }
   }
 
@@ -28,20 +28,51 @@ export default class StaticAlert extends Component {
     });
   }
 
-  renderAlert = () => (
-    <div opacity={this.state.opacity}
-         style={{border: '1px solid black', height: 200}}
-         onClick={this.handleClose}>
-      <p>WOOOOOOO</p>
-    </div>
-  )
-
   render() {
 
+    const { type, title, message } = this.props;
+    const infoCheck = type === 'Information' ? 'info' :'';
+
     return (
-      <div>
-        <p>Hi</p>
-        {this.state.isOpen && this.renderAlert()}
+      <div className="alertList">
+        <div className={`pe-alert alert-${type}`}>
+          {this.state.isOpen &&
+            <div opacity={this.state.opacity}
+                 className="pe-alert">
+
+              <button className="close-title"
+                      onClick={this.handleClose}
+                      aria-label="Close alert">
+                <Icon name="remove-sm-24" />
+              </button>
+
+              <div className="alert-content-container">
+                {type === 'Error' ? <span className="error-svg">
+                                      <svg focusable="false"
+                                           className="pe-icon--warning-24">
+                                           <use xlinkHref="#warning-24"></use>
+                                      </svg>
+                                    </span>
+                                  :null}
+                {type === 'Success' ? <span className="success-svg">
+                                        <svg focusable="false"
+                                             className="pe-icon--check-24">
+                                             <use xlinkHref="#check-sm-24"></use>
+                                        </svg>
+                                      </span>
+                                    :null}
+                <div className={`alert-content-${infoCheck}`}>
+                  <h4 className="pe-label alert-title">
+                    <strong>{title}</strong>
+                  </h4>
+                  <p className="pe-paragraph alert-text">
+                    {message}
+                  </p>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
       </div>
     )
   }
