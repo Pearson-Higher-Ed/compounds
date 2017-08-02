1623367f0e2604103ee7dd64a5087b1677d1d993
Chen, Hung <Hung.Chen@Pearson.com>
2017-08-02 09:20:50 -0400

    Updated demo documentation

--- demo/demoPages/DropdownPage.js
+++ demo/demoPages/DropdownPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Dropdown, DropdownItem } from '../../index';
+import { Dropdown, DropdownItem, Icon, Button } from '../../index';
 
 const simpleList = ['Thing one', 'Thing two'];
 const listItems = ['Pearson', 'Design', 'divider', 'Accelerator', '!!'];
 const mobileTitle = "Test title";
-
 const DropdownPage = () => (
     <div className="displaySection">
     <h2><a href="https://pearson-higher-ed.github.io/design/c/dropdown/beta/">Dropdown</a></h2>
@@ -14,95 +13,105 @@ const DropdownPage = () => (
         <div className="code">
           <h3>Props</h3>
           <h4>Required:</h4>
-          <ul>
-            <li className="li-props">dropdownControlLabel:String === "Descriptive text of what the button will be doing"</li>
-            <li className="li-props">presentationType:String === "label", "button", "icon"</li>
-            <li className="li-props">list:Array = ["one", "two", "three"] || ["one", "two", "divider", "three"]</li>
-            <li className="li-props">The "divider" option may be used in conjunction with any of
-            the presentationType for <br/>separating options presented in your list.</li>
-          </ul>
+            <li className="li-props">targetElement === element to be used as anchor of the dropdown</li>
           <h4>Optional:</h4>
           <ul>
-            <li className="li-props">presentationText:String === "Your label or button text"</li>
             <li className="li-props">mobileTitle:String === "Your mobile title"</li>
-            <li className="li-props">A different view is used for mobile and this title will be reflected
-              in the mobile header <br/>when the dropdown is open.</li>
-            <li className="li-props">dropup:Boolean === {`<Dropdown dropup />`}</li>
-            <li className="li-props">The dropdown menu can be changed to expand upwards
-              by using the <i>dropup</i> prop</li>
-            <li className="li-props">alignRight:Boolean === {`<Dropdown alignRight />`}</li>
-            <li className="li-props">By default, a dropdown menu is left aligned. Add the&nbsp;
-              <i>alignRight</i> prop to right align the <br/> dropdown menu.</li>
-            <li className="li-props">killFocus:Boolean === {`<Dropdown killFocus />`}</li>
-            <li className="li-props">The killFocus prop will assign a tabIndex of -1 inside of the list items.</li>
-            <li className="li-props">changeHandler:Function === {`<Dropdown changeHandler={(item) => { console.log(item) } }`}</li>
-            <li className="li-props">Callback that is fired when an item is selected in the Dropdown menu.</li>
+            <li className="li-props">
+              changeHandler:Function === {`<Dropdown changeHandler={(item) => { console.log(item) } }`}
+              <br/>
+              Callback that is fired when an item is selected in the Dropdown menu.
+            </li>
+            <li className="li-props">alignRight:Boolean === {`<Dropdown alignRight/>`}</li>
           </ul>
         </div>
 
         <h3>label /w Icon (non-selectable options)</h3>
           <Dropdown
             dropdownControlLabel="Dropdown open"
-            presentationType="label"
-            presentationText="label"
-            list={simpleList}
             mobileTitle={mobileTitle}
             changeHandler={(item) => {
               console.log(item);
             }}
+            targetElement={
+                <p> parent label</p>
+            }
           >
             <DropdownItem label="list item 1" />
             <DropdownItem divider />
             <DropdownItem label="list item 2" />
           </Dropdown>
           <p className="code">
-            {`const simpleList = ['Thing one', 'Thing two'];`} <br/>
             {`<Dropdown
-              dropdownControlLabel="Dropdown open"
-              presentationType="label"
-              presentationText="label"
-              list={simpleList}
-              mobileTitle="Mobile title"
-            />`}
+                dropdownControlLabel="Dropdown open"
+                mobileTitle={mobileTitle}
+                changeHandler={(item) => {
+                  console.log(item);
+                }}
+                targetElement={
+                    <p> parent label</p>
+                }
+              >
+                <DropdownItem label="list item 1" />
+                <DropdownItem divider />
+                <DropdownItem label="list item 2" />
+              </Dropdown>`}
           </p>
 
           <h3>button /w Icon (selectable options)</h3>
             <Dropdown
-              dropdownControlLabel="Button that opens Dropdown"
-              presentationType="button"
-              presentationText="button"
-              list={simpleList}
               mobileTitle={mobileTitle}
-            />
+              targetElement={
+                <Button>
+                   Role <Icon name="dropdown-open-sm-18">Button that opens Dropdown</Icon>
+                </Button>
+              }
+            >
+              <DropdownItem label="list item 1" />
+              <DropdownItem divider />
+              <DropdownItem label="list item 2" />
+            </Dropdown>
             <p className="code">
-              {`const simpleList = ['Thing one', 'Thing two'];`} <br/>
               {`<Dropdown
-                dropdownControlLabel="Button that opens Dropdown"
-                presentationType="button"
-                presentationText="button"
-                list={simpleList}
-                mobileTitle="Mobile title"
-              />`}
+                  mobileTitle={mobileTitle}
+                  targetElement={
+                    <Button>
+                       Role <Icon name="dropdown-open-sm-18">Button that opens Dropdown</Icon>
+                    </Button>
+                  }
+                >
+                  <DropdownItem label="list item 1" />
+                  <DropdownItem divider />
+                  <DropdownItem label="list item 2" />
+                </Dropdown>`}
             </p>
 
           <h3>Icon only (selectable options)</h3>
             <Dropdown
-              dropup
-              dropdownControlLabel="Icon that opens Dropdown"
-              presentationType="icon"
-              list={listItems}
               mobileTitle={mobileTitle}
-            />
+              targetElement={
+                <Button btnIcon>
+                   <Icon name="dropdown-open-sm-18">Button that opens Dropdown</Icon>
+                </Button>
+              }
+            >
+              <DropdownItem label="list item 1" />
+              <DropdownItem divider />
+              <DropdownItem label="list item 2" />
+            </Dropdown>
             <p className="code">
-              {`const listItems = ['Pearson', 'Design', 'divider', 'Accelerator', '!!'];`}
-              <br/>
               {`<Dropdown
-                dropup
-                dropdownControlLabel="Icon that opens Dropdown"
-                presentationType="icon"
-                list={listItems}
-                mobileTitle="Mobile title"
-              />`}
+                  mobileTitle={mobileTitle}
+                  targetElement={
+                    <Button btnIcon>
+                       <Icon name="dropdown-open-sm-18">Button that opens Dropdown</Icon>
+                    </Button>
+                  }
+                >
+                  <DropdownItem label="list item 1" />
+                  <DropdownItem divider />
+                  <DropdownItem label="list item 2" />
+                </Dropdown>`}
             </p>
 
       </div>
--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -2,20 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-import Icon from '../Icon';
 
 import './Dropdown.scss';
 
 export default class Dropdown extends Component {
 
   static propTypes = {
-    dropdownControlLabel: PropTypes.string.isRequired,
     mobileTitle: PropTypes.string,
-    presentationType: PropTypes.string.isRequired,
-    presentationText: PropTypes.string,
-    dropup: PropTypes.bool,
     alignRight: PropTypes.bool,
-    killFocus: PropTypes.bool,
     changeHandler: PropTypes.func
   };
 
@@ -30,7 +24,6 @@ export default class Dropdown extends Component {
     this.handleOutsideClick = this.handleOutsideClick.bind(this);
     this.toggleDropdown = this.toggleDropdown.bind(this);
     this.handleEsc = this.handleEsc.bind(this);
-    this.renderPresentation = this.renderPresentation.bind(this);
     this.itemSelected = this.itemSelected.bind(this);
   }
 
@@ -62,26 +55,6 @@ export default class Dropdown extends Component {
     }
   }
 
-  renderLabelPresentation() {
-    return (
-      <div
-        className="dropdown-activator"
-        onClick={this.toggleDropdown} >
-        <p className="dropdown-label-text">{this.props.presentationText}</p>
-      </div>
-    );
-  }
-
-  renderPresentation() {
-    switch (this.props.presentationType) {
-      case 'label':
-        return this.renderLabelPresentation();
-        break;
-      default:
-        break;
-    }
-  }
-
   itemSelected(e) {
     if (e.target.dataset.item !== 'divider') {
       this.props.changeHandler ? this.props.changeHandler(e.target.dataset.item) : null;
@@ -95,12 +68,15 @@ export default class Dropdown extends Component {
   render() {
     return (
         <div className="dropdown-container">
-          {this.renderPresentation()}
+          <div
+            className="dropdown-activator"
+            onClick={this.toggleDropdown} >
+            {this.props.targetElement}
+          </div>
           <ul className={this.state.open ? '' : 'dropdown-menu'} onClick={this.itemSelected}>
             {this.props.children}
           </ul>
         </div>
     )
   }
-
 };
