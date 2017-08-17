06fa4cc5a316b35cd724cc6a1c62451f75a4c717
Mike <Mhomolak@yahoo.com>
2017-08-17 15:18:48 -0600

    chore: Placeholder

--- demo/demoPages/PhoneNumberPage.js
+++ demo/demoPages/PhoneNumberPage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { PhoneNumber } from '../../index';
 
 const PhoneNumberPage = () => (
-  <PhoneNumber />
+  <div style={{padding: 15}}>
+    <h1>Phone Number</h1> <br/>
+
+    <PhoneNumber />
+  </div>
 )
 
 export default PhoneNumberPage;
--- src/PhoneNumber/PhoneNumber.js
+++ src/PhoneNumber/PhoneNumber.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Phone from 'react-phone-number-input';
+
+import rrui from 'react-phone-number-input/rrui.css';
+import rpni from 'react-phone-number-input/style.css';
 
 export default class PhoneNumber extends Component {
 
@@ -9,7 +13,12 @@ export default class PhoneNumber extends Component {
   render() {
 
     return (
-      <div>Hi</div>
+      <div>
+        <Phone
+          placeholder="Test text"
+          onChange={blah => console.log(blah)}
+        />
+      </div>
     )
   }
 }
