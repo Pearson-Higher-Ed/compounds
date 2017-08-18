04939449e5689ec4a2c695262cab02fefd950730
Mike <Mhomolak@yahoo.com>
2017-08-18 15:15:59 -0600

    chore: Prop setup, style tweaks

--- demo/demoPages/PhoneNumberPage.js
+++ demo/demoPages/PhoneNumberPage.js
@@ -5,7 +5,12 @@ const PhoneNumberPage = () => (
   <div style={{padding: 15}}>
     <h1>Phone Number</h1> <br/>
 
-    <PhoneNumber />
+    <PhoneNumber
+      country="US"
+      placeholder="555-555-5555"
+      labelText="Mobile phone"
+      onChange={(d) => console.log(d)}
+    />
   </div>
 )
 
--- src/PhoneNumber/PhoneNumber.js
+++ src/PhoneNumber/PhoneNumber.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Phone from 'react-phone-number-input';
 
-import rrui from 'react-phone-number-input/rrui.css';
-import rpni from 'react-phone-number-input/style.css';
+import './PhoneNumber.scss';
 
 export default class PhoneNumber extends Component {
+
+  static propTypes = {
+    labelText: PropTypes.string,
+    infoMessage: PropTypes.string,
+    errorMessage: PropTypes.string
+  }
 
   constructor(props) {
     super(props)
   }
 
   render() {
+    const { country, placeholder, onChange, value, onKeyDown, disabled,
+            onCountryChange, dictionary, countries, international, convertToNational,
+            selectMaxItems, style, inputStyle, className, inputClassName, labelText,
+            infoMessage, errorMessage } = this.props;
+
+    const errorLabel = errorMessage ? '--label_error' :'';
+    const errorInput = errorMessage ? 'react-phone-number-input__phone--error' : inputClassName;
 
     return (
       <div>
+
+        <label className={`pe-textLabelInput__label${errorLabel}`}>{labelText}</label>
+
         <Phone
-          placeholder="Test text"
-          onChange={blah => console.log(blah)}
+          value={value}
+          country={country}
+          placeholder={placeholder}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          disabled={disabled}
+          onCountryChange={onCountryChange}
+          dictionary={dictionary}
+          countries={countries}
+          international={international}
+          convertToNational={convertToNational}
+          selectMaxItems={selectMaxItems}
+          style={style}
+          inputStyle={inputStyle}
+          className={className}
+          inputClassName={errorInput}
         />
+        {infoMessage && <p className="pe-input--info_message">{infoMessage}</p>}
+        {errorMessage && <p className="pe-input--error_message">{errorMessage}</p>}
+
       </div>
-    )
+    );
   }
 }
