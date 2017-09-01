a5a936e53b5066ef70485832e230cc321b778b2b
Mike <Mhomolak@yahoo.com>
2017-09-01 12:25:43 -0600

    chore: Sort icons toggling

--- src/Table/components/TableHeaderCell.js
+++ src/Table/components/TableHeaderCell.js
@@ -1,50 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Icon from '../../Icon';
 
-// some check for columnSort; put the <Icon /> next to children
+export default class TableHeaderCell extends Component {
 
-const TableHeaderCell = (props, context) => {
-  const { children, scope, inputId, containerId, inputLabel, columnSort } = props;
-  const { table, selectable } = context;
+  static propTypes = {
+    children: PropTypes.node,
+    scope: PropTypes.string,
+    inputId: PropTypes.string,
+    containerId: PropTypes.string,
+    inputLabel: PropTypes.string,
+    columnSort: PropTypes.bool
+  }
 
-  return (
-    <th scope={scope}>
-      {
-        selectable && !children
-          ?  <div className="pe-checkbox" id={containerId}>
-               <input type="checkbox" id={inputId} />
-               <label htmlFor={inputId}>{inputLabel}</label>
-               <span>
-                 <Icon name="check-sm-18" />
-               </span>
-             </div>
-          : children
-      }
-      {
-        columnSort &&
-          <button type="button" className="pe-icon--btn">
-            <Icon name="sortable-18" />
-          </button>
-      }
-    </th>
-  )
-}
+  static defaultProps = {
+    scope: 'col',
+    columnSort: false
+  }
 
-export default TableHeaderCell;
+  constructor(props) {
+    super(props)
 
-TableHeaderCell.propTypes = {
-  children: PropTypes.node,
-  scope: PropTypes.string,
-  inputId: PropTypes.string,
-  containerId: PropTypes.string,
-  inputLabel: PropTypes.string,
-  columnSort: PropTypes.bool
-}
+    this.state = {
+      placeholder: 'check-sm-18'
+    }
+  }
 
-TableHeaderCell.defaultProps = {
-  scope: 'col',
-  columnSort: false
+  onClick = () => {
+    const { placeholder } = this.state;
+    if (placeholder === 'check-sm-18') {
+      this.setState({ placeholder: 'sort-up-18' })
+    } else if (placeholder === 'sort-up-18') {
+      this.setState({ placeholder: 'sort-down-18' })
+    } else if (placeholder === 'sort-down-18') {
+      this.setState({ placeholder: 'sort-up-18' })
+    }
+  }
+
+  render() {
+    const { children, scope, inputId, containerId, inputLabel, columnSort } = this.props;
+    const { table, selectable } = this.context;
+    const { placeholder } = this.state;
+
+    return (
+      <th>
+        {
+          selectable && !children
+            ?  <div className="pe-checkbox" id={containerId}>
+                 <input type="checkbox" id={inputId} />
+                 <label htmlFor={inputId}>{inputLabel}</label>
+                 <span>
+                   <Icon name="check-sm-18" />
+                 </span>
+               </div>
+            : children
+        }
+        {
+          columnSort &&
+            <button type="button" className="pe-icon--btn" onClick={this.onClick}>
+              <Icon name={placeholder} />
+            </button>
+        }
+      </th>
+    )
+  }
 }
 
 TableHeaderCell.contextTypes = {
